4cca35770a16e213cc248aebb9212f8c4b4ea6bd
Paweł <pawel.darulewski@gmail.com>
2020-03-30 16:52:43 +0200

    Removing slash from paths

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -11,10 +11,12 @@ class Preprocessor:
         self.path = local_path
 
     def convert_files(self):
-        command = 'for file in "{}"/*.webm; do ffmpeg -y -i "$file" -acodec pcm_s16le -ac 1 -ar 16000 -f wav "$file.wav"; done'.format(self.path)
+        path = os.path.join(self.path.replace(' ', r'\ '), '*.webm')
+        command = 'for file in {}; do ffmpeg -y -i "$file" -acodec pcm_s16le -ac 1 -ar 16000 -f wav "$file.wav"; done'.format(path)
+
         subprocess.call(command, shell=True)
 
-        files = glob.glob('{}/*.webm.wav'.format(self.path))
+        files = glob.glob(os.path.join(self.path, '*.webm.wav'))
 
         for file in files:
             os.rename(file, file.replace('.webm', ''))
@@ -22,10 +24,10 @@ class Preprocessor:
     def parse_json_to_csv(self, gender, age):
         # @author: tuethomsen28
 
-        with open('{}/db.json'.format(self.path)) as json_file:
+        with open(os.path.join(self.path, 'db.json')) as json_file:
             data = json.load(json_file)
 
-        with open('{}/overview.csv'.format(self.path), 'w', newline='') as csv_file:
+        with open(os.path.join(self.path, 'overview.csv'), 'w', newline='') as csv_file:
             csv_file.write('file,trans,gender,age\n')
             for j in data:
                 try:
@@ -39,7 +41,7 @@ class Preprocessor:
                     pass
 
     def split_set(self, test_size):
-        with open('{}/overview.csv'.format(self.path), 'r') as overview_file:
+        with open(os.path.join(self.path, 'overview.csv'), 'r') as overview_file:
             lines = overview_file.readlines()
             test_size = int((len(lines) - 1) * test_size)
 
@@ -48,18 +50,18 @@ class Preprocessor:
 
     def __create_split_directory(self, directory, lines):
         try:
-            shutil.rmtree('{}/{}'.format(self.path, directory))
+            shutil.rmtree(os.path.join(self.path, directory))
         except FileNotFoundError:
             pass
 
-        Path('{}/{}'.format(self.path, directory)).mkdir(parents=True, exist_ok=True)
+        Path(os.path.join(self.path, directory)).mkdir(parents=True, exist_ok=True)
 
-        with open('{0}/{1}/{1}.csv'.format(self.path, directory), 'w') as file:
+        with open(os.path.join(self.path, directory, '{}.csv'.format(directory)), 'w') as file:
             file.write('file,trans,gender,age\n')
 
             for line in lines:
                 file.write(line)
 
-                old_path = '{}/{}'.format(self.path, line.split(',')[0])
-                new_path = '{}/{}/{}'.format(self.path, directory, line.split(',')[0])
+                old_path = os.path.join(self.path, line.split(',')[0])
+                new_path = os.path.join(self.path, directory, line.split(',')[0])
                 os.rename(old_path, new_path)
